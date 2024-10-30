8867bfc17ccf1c80ee1ba46137d4b01228ce62c3
L3$L!3 <lesliealonzo200920097@gmail.com>
2024-10-29 22:56:27 -0600

    first commit

---
+++ creditos.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+   
+</head>
+<body>
+    
+
+
+
+<?php
+
+include ('header.php');
+
+?>
+
+
+<img src="images/Usac_logo.png" width="150" height="150">
+<img src="images/efpem.png" width="150" height="150" align="right">
+<br><br><br><br>
+<CENTEr> 
+<H1>Evaluación parcial 2 </H1>
+
+<ul class="list-group">
+<h3> 
+ <li class="list-group-item list-group-item-primary">LESLIE FABIOLA ALONZO ROSALES</li>
+  <li class="list-group-item list-group-item-secondary">CARNET: 200920097</li>
+  <li class="list-group-item list-group-item-success">LIC. Mynor Escobar</li>
+  </UL>
+  </h3>
+</CENTEr>
+
+
+
+
+
+ 
+  
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+</body>
+
+</html>
